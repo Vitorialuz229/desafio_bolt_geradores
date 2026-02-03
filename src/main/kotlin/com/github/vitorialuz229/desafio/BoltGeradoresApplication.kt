f11c57d1bc1aac80ccecfb90dd45ab9e3a15b9af
vitoria.luz <vitorialuz831@gmail.com>
2026-02-03 16:02:49 -0300

    chore: project initialization with Spring Boot and Kotlin

---
+++ src/main/kotlin/com/github/vitorialuz229/desafio/BoltGeradoresApplication.kt
@@ -0,0 +1,11 @@
+package com.github.vitorialuz229.desafio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoltGeradoresApplication
+
+fun main(args: Array<String>) {
+	runApplication<BoltGeradoresApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/vitorialuz229/desafio/BoltGeradoresApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.vitorialuz229.desafio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoltGeradoresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
