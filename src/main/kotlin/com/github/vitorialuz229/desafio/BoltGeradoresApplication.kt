fbcdeb4da9939b44f82ee2cb18ef056d02218b1b
vitoria.luz <vitorialuz831@gmail.com>
2026-02-04 15:56:08 -0300

    chore: update project configuration and documentation

--- src/main/kotlin/com/github/vitorialuz229/desafio/BoltGeradoresApplication.kt
+++ src/main/kotlin/com/github/vitorialuz229/desafio/BoltGeradoresApplication.kt
@@ -2,8 +2,10 @@ package com.github.vitorialuz229.desafio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class BoltGeradoresApplication
 
 fun main(args: Array<String>) {
